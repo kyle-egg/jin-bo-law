73e22a07e980e55e92db78adb809c4cf260d2b89
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2021-11-03 17:39:00 +0000

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,24 @@
 function NavBar() {
 
   return (
-    <div className="uk-position-relative">
-      <img src="images/light.jpg" alt=""/>
-      <div className="uk-position-top">
-        <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar>
-          <div className="uk-navbar-left">
-            <a className="uk-navbar-item uk-logo" href="#">Logo</a>
-            <ul className="uk-navbar-nav">
-              <li className="uk-active"><a href="#">Active</a></li>
-              <li><a href="#">Item</a></li>
-              <li>
-                <a href="#">Parent</a>
-                <div className="uk-navbar-dropdown">
-                  <ul className="uk-nav uk-navbar-dropdown-nav">
-                    <li className="uk-active"><a href="#">Active</a></li>
-                    <li><a href="#">Item</a></li>
-                    <li className="uk-nav-header">Header</li>
-                    <li><a href="#">Item</a></li>
-                    <li><a href="#">Item</a></li>
-                    <li className="uk-nav-divider"></li>
-                    <li><a href="#">Item</a></li>
-                  </ul>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
+    <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar>
+      <div className="uk-navbar-left">
+        <a className="uk-navbar-item uk-logo" href="/"><img className="logo" src="https://github.com/kyle-egg/jin-bo-law/blob/main/src/assets/jin-logo-gold.png?raw=true"/></a>
+        <ul className="uk-navbar-nav">
+          <li><a href="/whatson">What&apos;s On</a></li>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/giftvouchers">Gift Vouchers</a></li>
+          <li><a href="/events">Private Events</a></li>
+          <li><a href="https://my.matterport.com/show/?m=3LVPz6vi2vA">Virtual Tour</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+        <div className="uk-navbar-right">
+          <ul className="uk-navbar-nav">
+            <li className="uk-active booknav"><a href="/book">Book Now</a></li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </nav>
   )
 }
 
