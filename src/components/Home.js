8459468c1e3bc91d25f4d3d7380e3f527d3d7025
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2022-02-07 12:32:59 +0000

    style changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,22 @@
+import Image from '../assets/jin-logo-gold.png'
+
 function Home() {
   return (
     <section>
       <div id="offcanvas-nav-primary" uk-offcanvas="flip: true; overlay: true">
         <div className="uk-offcanvas-bar uk-flex uk-flex-column">
-          <button className="uk-offcanvas-close" type="button" uk-close>X</button>
+          <button id="closebutton" className="uk-offcanvas-close" type="button" uk-close>X</button>
           <ul className="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
-            <li><a href="/whatson">What&apos;s On</a></li>
-            <li><a href="/menu">Menu</a></li>
-            <li><a href="/giftvouchers">Gift Vouchers</a></li>
-            <li><a href="/events">Private Events</a></li>
-            <li><a href="https://my.matterport.com/show/?m=3LVPz6vi2vA">Virtual Tour</a></li>
-            <li><a href="#">Contact Us</a></li>
+            <img className="sidenavlogo" src={Image}/>
             <li className="uk-nav-divider"></li>
-            <li><a href="/book">Book Now</a></li>
+            <li><a id= "navtext" href="/whatson">What&apos;s On</a></li>
+            <li><a id= "navtext" href="/menu">Menu</a></li>
+            <li><a id= "navtext" href="/giftvouchers">Gift Vouchers</a></li>
+            <li><a id= "navtext" href="/events">Private Events</a></li>
+            <li><a id= "navtext" href="https://my.matterport.com/show/?m=3LVPz6vi2vA">Virtual Tour</a></li>
+            <li><a id= "navtext" href="#">Contact Us</a></li>
+            <li className="uk-nav-divider"></li>
+            <li><a id= "navtext" href="/book">Book Now</a></li>
           </ul>
         </div>
       </div>
@@ -32,45 +36,7 @@ function Home() {
         </div>
       </section>
       <section>
-        <div className="uk-child-width-1-3@l uk-grid-small uk-grid-match" uk-grid>
-          <div>
-            <div className="uk-card uk-card-default uk-card-body">
-              <h3 className="uk-card-title">Default</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </div>
-          </div>
-          <div>
-            <div className="uk-card uk-card-primary uk-card-body">
-              <h3 className="uk-card-title">Primary</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </div>
-          </div>
-          <div>
-            <div className="uk-card uk-card-secondary uk-card-body">
-              <h3 className="uk-card-title">Secondary</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </div>
-          </div>
-        </div>
-        <div className="uk-child-width-1-3@m" uk-grid uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 500; repeat: true">
-          <div>
-            <div className="uk-card uk-card-default uk-card-body">
-              <h3 className="uk-card-title">Fade</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
-          <div>
-            <div className="uk-card uk-card-default uk-card-body">
-              <h3 className="uk-card-title">Fade</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
-          <div>
-            <div className="uk-card uk-card-default uk-card-body">
-              <h3 className="uk-card-title">Fade</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
+        <div className="uk-child-width-1-3@s uk-flex" uk-grid uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 250; repeat: true">
           <div>
             <div className="uk-card uk-card-default uk-card-body">
               <h3 className="uk-card-title">Fade</h3>
